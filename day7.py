885d339c0e12a226ca4d3c13162784a2e5e5ae90
Marine Greff <marine.greff@protonmail.com>
2020-12-19 15:01:06 +0100

    Finished day7 part1 started part2

---
+++ day7.py
@@ -0,0 +1,66 @@
+inputFile = open("input/day7.txt", "r")
+inputLines = inputFile.readlines()
+
+listBagsWithoutShinyGold = []
+listBagsWithShinyGold = []
+listUncertain = []
+
+def goldMiner():
+    for line in inputLines:
+        splittedLine = line.split()
+        bagColor = splittedLine[0] + splittedLine[1]
+        childrenBags = []
+
+        if splittedLine[4] == "no" or bagColor == "shinygold":
+            if bagColor not in listBagsWithoutShinyGold:
+                listBagsWithoutShinyGold.append(bagColor)
+        else:
+            someChildrenBags = True
+            counter = 5
+            while someChildrenBags:
+                childrenBags.append(splittedLine[counter] + splittedLine[counter + 1])
+                if (splittedLine[counter + 2] == "bag.") or (splittedLine[counter + 2] == "bags."):
+                    someChildrenBags = False
+                counter = counter + 4
+            if "shinygold" in childrenBags:
+                if bagColor not in listBagsWithShinyGold:
+                    listBagsWithShinyGold.append(bagColor)
+            else:
+                # Look into every children to see if they can have the bag
+                for childrenBag in childrenBags:
+                    if childrenBag in listBagsWithShinyGold:
+                        if bagColor not in listBagsWithShinyGold:
+                            listBagsWithShinyGold.append(bagColor)
+                    else:
+                        if bagColor not in listUncertain:
+                            listUncertain.append(bagColor)
+
+goldMiner()
+lastRunCount = 0
+while lastRunCount != len(listBagsWithShinyGold):
+    lastRunCount = len(listBagsWithShinyGold)
+    goldMiner()
+
+print("Bags that can eventually contain one shiny gold bag {}".format(len(listBagsWithShinyGold)))
+
+def bagCounter(color):
+    for line in inputLines:
+        splittedLine = line.split()
+        bagColor = splittedLine[0] + splittedLine[1]
+        
+        if bagColor == color:
+            childrenBags = []
+            someChildrenBags = True
+            counter = 4
+            while someChildrenBags:
+                childrenBags.append((splittedLine[counter + 1] + splittedLine[counter + 2], int(splittedLine[counter])))
+                if (splittedLine[counter + 3] == "bag.") or (splittedLine[counter + 3] == "bags."):
+                    someChildrenBags = False
+                counter = counter + 4
+            print("Childern bags {}".format(childrenBags))
+            return childrenBags
+            
+nestedBags = bagCounter("shinygold")
+
+
+inputFile.close()
